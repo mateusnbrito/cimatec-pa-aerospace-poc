51b4759dd58367facbe94f964390950b30390fc8
mateusnbrito <mateusnascbrito@gmail.com>
2025-03-11 01:52:17 -0300

    add model saving

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 import timm
 import torch.optim as optim
+import matplotlib.pyplot as plt
+from os import listdir
+from pathlib import Path
 from torch.utils.data import Dataset, DataLoader
 from torchvision.datasets import ImageFolder
 from tqdm import tqdm
+from PIL import Image
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+MODEL_FILENAME = "aerospace_model.pt"
 TRAIN_IMAGES_PATH = "dataset/train"
 TEST_IMAGES_PATH = "dataset/test"
+PREDICTIONS_IMAGE_PATH = "imagens-de-teste"
 BATCH_SIZE = 32
 EPOCHS = 1
 NUMBER_OF_CLASSES = 6
@@ -25,6 +31,7 @@ criterion = None
 optimizer = None
 train_loader = None
 classes = None
+hasTrainedModel = False
 
 class ImageDataset(Dataset):
   def __init__(self, train_images_dir, transform=None):
@@ -60,6 +67,35 @@ class ImageClassifer(nn.Module):
 
     return output
 
+def preprocess_image(image_path, transform):
+  image = Image.open(image_path).convert("RGB")
+
+  return image, transform(image).unsqueeze(0)
+
+def predict(model, image_tensor, device):
+  model.eval()
+
+  with torch.no_grad():
+    image_tensor = image_tensor.to(device)
+    outputs = model(image_tensor)
+    probabilities = torch.nn.functional.softmax(outputs, dim=1)
+
+  return probabilities.cpu().numpy().flatten()
+
+def visualize_predictions(original_image, probabilities, class_names):
+  fig, axarr = plt.subplots(1, 2, figsize=(14, 7))
+
+  axarr[0].imshow(original_image)
+  axarr[0].axis("off")
+
+  axarr[1].barh(class_names, probabilities)
+  axarr[1].set_xlabel("Probabilidade")
+  axarr[1].set_title("Predições de Classes")
+  axarr[1].set_xlim(0, 1)
+
+  plt.tight_layout()
+  plt.show()
+
 def train():
   global model
   global criterion
@@ -74,7 +110,7 @@ def train():
 
     running_loss = 0.0
 
-    for images, labels in tqdm(train_loader, desc="Training loop"):
+    for images, labels in tqdm(train_loader, desc="Treinando o modelo..."):
       images, labels = images.to(DEVICE), labels.to(DEVICE)
 
       optimizer.zero_grad()
@@ -91,7 +127,7 @@ def train():
     running_loss = 0.0
 
     with torch.no_grad():
-      for images, labels in tqdm(test_loader, desc="Test loop"):
+      for images, labels in tqdm(test_loader, desc="Testando o modelo..."):
         images, labels = images.to(DEVICE), labels.to(DEVICE)
       
         outputs = model(images)
@@ -101,7 +137,23 @@ def train():
     val_loss = running_loss / len(test_loader.dataset)
     val_losses.append(val_loss)
 
+    torch.save(model.state_dict(), MODEL_FILENAME)
+
     print(f"Epoch { epoch + 1 }/{ EPOCHS } - Perda (Treino): { train_loss }, Perda (Teste): { val_loss }")
+
+def makePredictions():
+  global image_transformer
+  global model
+
+  for image_name in listdir(PREDICTIONS_IMAGE_PATH):
+    image_path = f"{PREDICTIONS_IMAGE_PATH}/{image_name}"
+
+    image, image_tensor = preprocess_image(image_path, image_transformer)
+    probabilities = predict(model, image_tensor, DEVICE)
+
+    class_names = dataset.classes
+
+    visualize_predictions(image, probabilities, class_names)
 
 def main():
   global image_transformer
@@ -113,6 +165,15 @@ def main():
   global train_loader
   global test_loader
   global classes
+  global hasTrainedModel
+
+  model_file = Path(MODEL_FILENAME)
+  model = ImageClassifer(num_classes=NUMBER_OF_CLASSES)
+
+  if model_file.is_file():
+    model.load_state_dict(torch.load(MODEL_FILENAME))
+
+    hasTrainedModel = True
 
   image_transformer = transforms.Compose([
     transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
@@ -121,18 +182,18 @@ def main():
 
   dataset = ImageDataset(TRAIN_IMAGES_PATH, image_transformer)
   dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
-  model = ImageClassifer(num_classes=NUMBER_OF_CLASSES)
   criterion = nn.CrossEntropyLoss()
   optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
   classes = {v: k for k, v in ImageFolder(TRAIN_IMAGES_PATH).class_to_idx.items()}
-
   train_dataset = ImageDataset(TRAIN_IMAGES_PATH, transform=image_transformer)
   test_dataset = ImageDataset(TEST_IMAGES_PATH, transform=image_transformer)
-
   train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
   test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-  train()
+  if not hasTrainedModel:
+    train()
+
+  makePredictions()
 
 if __name__=="__main__":
   main()
